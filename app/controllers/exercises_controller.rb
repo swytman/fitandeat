8604493b55e6c5b859483dba587f6f95753ceb07
swytman <swytman@gmail.com>
2016-03-31 23:35:06 +0300

    alert

--- app/controllers/exercises_controller.rb
+++ app/controllers/exercises_controller.rb
@@ -1,21 +1,36 @@
 class ExercisesController < ApplicationController
 
   before_action :set_chapter
+  before_action :get_item, only: [:show, :edit, :destroy, :update]
 
   def index
-    @exercises = Exercise.all
+    @items = Exercise.all
   end
 
   def create
+    item = Exercise.new(exercise_params)
+    if item.save
+      redirect_to edit_exercise_path(item), notice: 'Создано'
+    else
+      render action: 'new', error: 'Ошибка при добавлении'
+    end
+  end
+
+  def edit
+
 
   end
 
   def update
-
+    if @item.update(exercise_params)
+      redirect_to edit_exercise_path(@item), notice: 'Сохранено'
+    else
+      render action: 'edit', error: 'Ошибка сохранения'
+    end
   end
 
   def new
-    @exercise = Exercise.new
+    @item = Exercise.new
   end
 
   def destroy
@@ -24,6 +39,16 @@ class ExercisesController < ApplicationController
 
   def set_chapter
     @chapter = {title: 'Упражнения', src: exercises_path}
+  end
+
+  private
+
+  def exercise_params
+    params[:exercise].permit(:title, :description)
+  end
+
+  def get_item
+    @item = Exercise.find(params[:id])
   end
 
 end
