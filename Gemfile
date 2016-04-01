9188d4769d77fe7dd3a7089819152075da9c6d31
swytman <swytman@gmail.com>
2016-04-01 22:32:43 +0300

    add capistrano

--- Gemfile
+++ Gemfile
@@ -6,6 +6,7 @@ gem 'slim'
 gem 'rails', '4.2.4'
 # Use sqlite3 as the database for Active Record
 gem 'pg'
+gem 'unicorn', group: :production
 # Use SCSS for stylesheets
 gem 'sass-rails', '~> 5.0'
 # Use Uglifier as compressor for JavaScript assets
@@ -42,8 +43,10 @@ end
 group :development do
   # Access an IRB console on exception pages or by using <%= console %> in views
   gem 'web-console', '~> 2.0'
-
-  # Spring speeds up development by keeping your application running in the background. Read more: https://github.com/rails/spring
-  gem 'spring'
+  gem 'capistrano', '~> 3.1'
+  gem 'capistrano-rails', '~> 1.1'
+  gem 'capistrano-bundler'
+  gem 'capistrano3-unicorn'
+  gem 'capistrano-rvm'
 end
 
---
+++ config/deploy.rb
@@ -0,0 +1,51 @@
+set :application, 'fitandeat.ru'
+set :repo_url, 'https://github.com/swytman/fitandeat.git'
+set :deploy_to, '/var/www/fitandeat'
+# set :rollbar_token, 'e91415aea6404844afe7f709d62a76b5'
+# set :rollbar_env, Proc.new { fetch :stage }
+# set :rollbar_role, Proc.new { :app }
+set :current_path,  "#{fetch(:deploy_to)}/current"
+set :unicorn_conf, "#{fetch(:deploy_to)}/current/config/unicorn/#{fetch(:stage)}.rb"
+set :unicorn_pid, "#{fetch(:deploy_to)}/shared/pids/unicorn.pid"
+set :linked_files, %w{config/database.yml config/api_keys.yml config/mail.yml}
+set :linked_dirs, %w{public/system}
+
+# UNICORN
+
+
+#NGINX
+namespace :nginx do
+  task :restart do
+    on roles(:web) do
+      execute "sudo service nginx restart"
+    end
+  end
+  task :start do
+    on roles(:web) do
+      execute "sudo service nginx start"
+    end
+  end
+  task :stop do
+    on roles(:web) do
+      execute "sudo service nginx stop"
+    end
+  end
+
+end
+
+# DB
+namespace :db do
+  task :migrate do
+    on roles(:db) do
+      execute "rake db:migrate RAILS_ENV=#{fetch(:rails_env)} --trace"
+    end
+  end
+end
+
+after 'deploy:publishing', 'deploy:restart'
+
+namespace :deploy do
+  task :restart do
+    invoke 'unicorn:restart'
+  end
+end
