dbca92a6500d772a262ebac639609da3401597c7
swytman <swytman@gmail.com>
2016-04-07 23:30:46 +0300

    fix

--- app/serializers/day_exercise_serializer.rb
+++ app/serializers/day_exercise_serializer.rb
@@ -2,7 +2,7 @@ class DayExerciseSerializer < ActiveModel::Serializer
   attributes :title, :order, :description
 
   def title
-    return unless object.title
+    return unless object.exercise
     result = "#{object.exercise.title}"
     if object.count? && object.count > 0
       result = "#{result} - #{object.count}"
