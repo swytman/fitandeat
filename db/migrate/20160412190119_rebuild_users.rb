5bf05f99f42a4e5a14cd17a7d7c3270b0877a274
Stepanov Viacheslav <stepanov.vm@gmail.com>
2016-04-18 18:35:09 +0300

    migrations

--- db/migrate/20160412190119_rebuild_users.rb
+++ db/migrate/20160412190119_rebuild_users.rb
@@ -1,6 +1,6 @@
 class RebuildUsers < ActiveRecord::Migration
   def change
-    #drop_table :users
+    drop_table :users if User.table_exists?
     create_table(:users) do |t|
       t.integer :telegram_id
       t.integer :telegram_name
